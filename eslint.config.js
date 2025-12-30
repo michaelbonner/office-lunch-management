9222532e5dadc9fb08c21e78914ec8e49d6e3e2f
Michael Bonner <michaelwbonner@gmail.com>
2025-12-30 15:02:26 -0700

    prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,12 @@
-import prettier from 'eslint-config-prettier';
-import { fileURLToPath } from 'node:url';
+import svelteConfig from './svelte.config.js';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
+import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.js';
 
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
