49a2a02e237243b0d57faba81c85ecd51f014e40
Michael Bonner <michaelwbonner@gmail.com>
2026-01-14 15:24:57 -0700

    Update fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import { Toaster } from '$lib/components/ui/sonner/index.js';
 	import '../app.css';
 	import './layout.css';
+	import '@fontsource-variable/inter/wght.css';
 
 	let { children } = $props();
 </script>
@@ -14,3 +15,15 @@
 <Toaster />
 
 {@render children?.()}
+
+<style>
+	:global(body) {
+		font-family: 'Inter Variable', sans-serif;
+		margin: 0;
+	}
+
+	:global(h1, h2, h3, h4, h5, h6) {
+		font-family: 'Inter Variable', sans-serif;
+		font-weight: 600;
+	}
+</style>
